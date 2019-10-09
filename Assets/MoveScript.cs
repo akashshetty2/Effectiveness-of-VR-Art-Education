15c458e68808e48fba0eca2eaa95ef41ae864c9c
akashshetty2 <55859646+akashshetty2@users.noreply.github.com>
2019-10-09 14:15:11 -0400

    Add files via upload

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public float speed = 5.0f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) {
+
+        }
+    }
+}
