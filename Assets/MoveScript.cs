3891ba58305d26cc25ec5886987a90aea5d751fd
akashshetty2 <55859646+akashshetty2@users.noreply.github.com>
2019-10-11 13:29:38 -0400

    Add files via upload

--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class MoveScript : MonoBehaviour
 {
+    float speed = 7.0f; 
+
     // Start is called before the first frame update
     void Start()
     {
-        public float speed = 5.0f; 
+       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.RightArrow) {
+// Update is called once per frame
+void Update()
+{
+        //move with respect to the x-z plane 
+        Vector3 change = new Vector3(Input.GetAxis("Horizontal"),
+           0, Input.GetAxis("Vertical"));
 
-        }
-    }
+        //transform the positoin of the camera depending on the user input 
+        transform.position = transform.position + (change * speed *
+            Time.deltaTime);
+}
+    
+       
+
 }
