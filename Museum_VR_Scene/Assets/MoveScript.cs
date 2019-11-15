aa766e6b444f728e62a62ec9e5cc019d4784314c
Akash <akash.kr.shetty@gmail.com>
2019-11-14 22:53:31 -0500

    Unity scene added

---
+++ Museum_VR_Scene/Assets/MoveScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    float speed = 7.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+// Update is called once per frame
+void Update()
+{
+        //move with respect to the x-z plane 
+        Vector3 change = new Vector3(Input.GetAxis("Horizontal"),
+           0, Input.GetAxis("Vertical"));
+
+        //transform the positoin of the camera depending on the user input 
+        transform.position = transform.position + (change * speed *
+            Time.deltaTime);
+}
+    
+       
+
+}
